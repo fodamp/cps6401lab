45aac2e596ce3100ff49922063f859dcf6ce8b5b
fodamp <fowusudamp@miners.utep.edu>
2024-10-24 16:02:55 -0600

    Adding Newton's Method

--- switchcase.c
+++ switchcase.c
@@ -2,16 +2,25 @@
 #include <math.h>
 
 
+//Global Variables
+double tol = 1e-8;
+int max_iter = 50;
+double x0;
+double x1;
+double x2 = 0;
+int counter = 0;
+
+
 // Soving for the function
 double fun(double x)
 {
-return (pow(x,6) -x - 1.0);
+return (pow(x,6) - x - 1.0);
 }
 
 // Solving for the derivative
 double derivfun(double x)
 {
-return (6.0 * pow(x,5) - 1.0);
+return (6.0 * pow(x,5) - 1);
 }
 
 
@@ -28,36 +37,58 @@ return root;
 // Newton's Method
 double NewtonMet()
 {
-    
-double root;
-printf("Input the function: ");
+
+printf("\nSolving Equation Using Newton's Method\n");
+printf("\nEnter Initial Number: \t");
+scanf("%lf", &x0);  
+
+
+do 
+{
+	if (counter == max_iter)
+	{
+	printf("\nMax iterations reached. No convergence\n");
+	break;
+	}
+	
+	if (derivfun(x0) == 0 )
+	{
+	printf("\nNo root found\n");
+	break;
+	}
+	
+	x2 = x0 - (fun(x0)/derivfun(x0));
+	counter = counter + 1;
+	printf("At iteration = %d of maxiterations= %d |f(x2)| = %lf\n ", counter, max_iter, fabs(fun(x2)));
+	x0 = x2;
+
+} while (fabs(fun(x2)) > tol); 
+
+printf("\nThe approximate root is %lf with tolerance = %lf using %d iterations\n ", x2, tol, counter); 
+
+//printf("Input the function: ");
 //scanf();
-return root;
+//return root;
 
 }
 
 double SecantMet()
 {
 
-double tol = 0.0001;
-int max_iter = 50;
-double x0;
-double x1;
-double x2 = 0;
-int counter = 0;
+
 //int option = 3;
 //double root;
 
-printf("\n Solving Equation Using The Secant Method \n");
-printf("\n Enter initial First Number: \n");
+printf("\nSolving Equation Using The Secant Method \n");
+printf("\nEnter initial First Number: \n");
 scanf("%lf", &x0);
 
-printf("\n Enter Initial Second Number: \n");
+printf("\nEnter Initial Second Number: \n");
 scanf("%lf", &x1);
 
 if (fun(x0) == fun(x1))
 {
-	printf("funct(%f) = funct(%f). Please input different initial values \n", x0, x1);
+	printf("funct(%f) = funct(%f). Please input different initial values\n", x0, x1);
 	//return;
 	//break;
 }
@@ -68,11 +99,11 @@ else
 	{
 	x2 = (x0 * fun(x1) - x1 * fun(x0))/(fun(x1) - fun(x0));
 	counter = counter + 1;
-	printf("At iteration = %d of maxiterations= %d |f(x2)| = %lf  \n ", counter, max_iter, fabs(fun(x2)));
+	printf("At iteration = %d of maxiterations= %d |f(x2)| = %lf\n ", counter, max_iter, fabs(fun(x2)));
 	
 	if(counter == max_iter)
 	{
-	printf("Max iterations reached. No convergence \n");
+	printf("Max iterations reached. No convergence\n");
 	break;
 	}
 	
@@ -82,7 +113,7 @@ else
 	} while(fabs(fun(x2)) > tol);
 	
 	
-	printf("\n The approximate root is %lf with tolerance = %lf using %d iterations \n ", x2, tol, counter);	
+	printf("\nThe approximate root is %lf with tolerance = %lf using %d iterations\n ", x2, tol, counter);	
 
 }
 		
