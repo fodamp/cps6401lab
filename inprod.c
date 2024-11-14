a87661e45f400d8e42fd5247f753d1eae21d7391
fodamp <fowusudamp@miners.utep.edu>
2024-11-13 17:18:38 -0700

    Update 2

--- inprod.c
+++ inprod.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h> 
+#include <math.h>
 #define a_size 5
 
 
@@ -7,15 +9,24 @@ int main(int argc, char* argv[])
 {
   clock_t start, end;
   double cpu_time_used;
-  double x[a_size] = {1., 2., 3., 4., 5.};
-  double y[a_size] = {1., -1., 1., -1., 1.};
   double dotproduct = 0.0;
+  int rowsize = atoi(argv[1]); 
+  //int colsize = atoi(argv[1]);
+  int x[rowsize];
+  int y[rowsize];
+  
+  
      
      
   start = clock();
      /* Do the work. */
-     for (int i = 0; i< a_size; i++)
-     	dotproduct += x[i] * y[i];
+     
+     for (int i = 0; i< rowsize; ++i)
+     {
+     	x[i] = (-1)^(i);
+     	y[i] = (-1)^(i+1);
+     	dotproduct = dotproduct +  (x[i] * y[i]);
+     }	
  
   end = clock();
   cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
